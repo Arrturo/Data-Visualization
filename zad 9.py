ccb3f7b69deeeee031bb1f188e00a5bb4f752de9
Artur <Artczi@users.noreply.github.com>
2021-02-27 21:19:02 +0100

    Lab_01

--- zad 9.py
+++ zad 9.py
@@ -4,6 +4,6 @@ szestnastkowe = hex(190442)
 
 print('%(zm1)s' % {'zm1': string})
 print('%(zm2)f' % {'zm2': float})
-print('%(zm3)s'.format() % {'zm3': szestnastkowe})
+print('%(zm3)s' % {'zm3': szestnastkowe})
 
 
