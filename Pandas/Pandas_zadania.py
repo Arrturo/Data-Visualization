4f15a4ee8a7deea753e5be5cb2a6b48eca70ae31
Artur <Artczi@users.noreply.github.com>
2021-05-04 21:00:21 +0200

    ..

--- Pandas/Pandas_zadania.py
+++ Pandas/Pandas_zadania.py
@@ -5,19 +5,23 @@ import xlrd
 import openpyxl
 
 ##1
-# df = pd.read_excel('imiona.xlsx')
+df = pd.read_excel('imiona.xlsx')
 # print(df)
 
 ##2
 # print(df[df['Liczba'] < 1000])
 
-# print(df.groupby(['Imie']))
+# print(df[df["Imie"] == "Artur"]
 
+# print(df["Liczba"].sum())
+
+# df_4 = df[(df["Rok"] >= 2005) & (df["Rok"] <= 2010)]
+# print(df_4["Rok"].sum())
 
 
 
 
 ##3
-df = pd.read_csv('zamowienia.csv', header=0, sep=';')
-print(df)
+# df = pd.read_csv('zamowienia.csv', header=0, sep=';')
+# print(df)
 
